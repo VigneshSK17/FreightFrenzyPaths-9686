b474202fc423812f49eb91071968d1614aa2e72b
Vignesh Suresh Kumar <vigneshsureshkumar17@gmail.com>
2021-11-26 19:46:21 -0500

    Added Freight Frenzy field png

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -39,7 +39,7 @@ class App : Application() {
 
     override fun start(stage: Stage?) {
         this.stage = stage!!
-        fieldImage = Image("/field.png")
+        fieldImage = Image("/field-2021-official.png")
 
         val root = Group()
 
